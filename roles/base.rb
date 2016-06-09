6c8a442544a7c73d9c0ed1ca180ece95a6974c3b
“mpaturu1” <mpaturu@gmail.com>
2016-06-09 07:25:48 -0500

    First Commit June 9th

--- roles/base.rb
+++ roles/base.rb
@@ -1,6 +1,6 @@
 name "base"
 description "this is base role"
 default_attributes "mp_attributes" => { "mymsg" => "Hello World. From Base Role" }
-run_list "recipe[mp_attributes]"
+run_list "recipe[chef-client::cron]","recipe[mp_attributes]"
 
 
